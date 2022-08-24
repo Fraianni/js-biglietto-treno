0af67472fdeab50a05c996d70ffe3a44c3b3abc8
Fraianni <francescoianni1995@gmail.com>
2022-08-24 09:59:27 +0200

    aggiunta controllo su age && su km

--- javascript/script.js
+++ javascript/script.js
@@ -4,13 +4,24 @@ const km = parseInt(prompt('inserire km da percorrere'));
 
 let costo_tot = costo_per_km * km;
 
-if (age < 18) {
-    costo_tot = costo_tot - (costo_tot * 0.2);
+if (isNaN(age) || isNaN(km)) {
+    alert("Dato/i inserito/i non valido/i");
 }
 
-else if (age > 65) {
-    costo_tot = costo_tot - (costo_tot * 0.4);
+else {
+    if (age < 18) {
+        costo_tot = costo_tot - (costo_tot * 0.2);
+    }
+
+    else if (age > 65) {
+        costo_tot = costo_tot - (costo_tot * 0.4);
+    }
+
+    alert("il costo totale per la persona di età " + age + " che percorre " + km + "km è di " + costo_tot.toFixed(2) + "euro");
+
+
 }
 
-alert("il costo totale per la persona di età " + age + " che percorre " + km + "km è di " + costo_tot.toFixed(2) + "euro");
+
+
 
